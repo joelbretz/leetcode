3ae35ccfaa75b439f3799eef5db0623b92168ab7
Joel B <joelbretz@users.noreply.github.com>
2022-07-01 13:10:52 -0500

    Update NumberofOperationToMakeNetworkConnected.cpp

--- NumberofOperationToMakeNetworkConnected.cpp
+++ NumberofOperationToMakeNetworkConnected.cpp
@@ -23,7 +23,50 @@ class Solution {
   }
 
   public:
+  
+  // BFS version
   int makeConnected(int n, vector<vector<int>>& connections) {
+  
+    int edges = connections.size();
+    if(n - 1 > edges)
+      return -1;
+  
+    // create an adjacency list from the list of connection pairs
+    vector<vector<int>> adj(n);
+    for(auto c : connections) {
+      adj[c[0]].push_back(c[1]);
+      adj[c[1]].push_back(c[0]);
+    }
+    // visited vector
+    vector<bool> visited(n, false);
+    
+    queue<int> q;
+    int subnets = 0;
+    
+    // loop over all nodes
+    for(int i = 0; i < n; ++i) {
+      if(!visited[i]) {
+        subnets++;
+        visited[i] = true;
+        q.push(i);
+        while(!q.empty()) {
+          int node = q.front();
+          q.pop();
+          for(auto neighbor : adj[node]) {
+            if(!visited[neighbor]) {
+              q.push(neighbor);
+              visited[neighbor] = true;
+            }
+          }
+        }
+      }
+    }
+    
+    return subnets-1;
+  }
+  
+  
+  int makeConnectedDFS(int n, vector<vector<int>>& connections) {
     
     //cout << "------------------" << endl;
 
