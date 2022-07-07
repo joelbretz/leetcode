d2616b6bf65fe2065b3a1835eed47910f8880da6
Joel B <joelbretz@users.noreply.github.com>
2022-07-07 00:37:04 -0500

    Create ConvertSortedArrayToBinarySearchTree.cpp

---
+++ ConvertSortedArrayToBinarySearchTree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  protected:
+  TreeNode* helper(vector<int>& nums, int l, int r) {
+  
+    if(l > r)
+      return nullptr;
+    
+    int mid = (l + r) / 2; 
+    TreeNode *node = new TreeNode(nums[mid], 
+                                  helper(nums, l, mid - 1), 
+                                  helper(nums, mid + 1, r));
+    return node;
+  }
+
+  public:
+  TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return helper(nums, 0, nums.size() - 1);
+  }
+};
