5cd932437d2484a06099b5f14b8f735992fa0b89
Joel B <joelbretz@users.noreply.github.com>
2022-09-06 12:01:36 -0500

    Create BinaryTreePruning.cpp

---
+++ BinaryTreePruning.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  public:
+  TreeNode* pruneTree(TreeNode* root) {
+    if(root == nullptr)
+      return root;
+    
+    root->left = pruneTree(root->left);
+    root->right = pruneTree(root->right);
+    
+    // return null on zero leaves
+    if(root->val == 0 && root->left == nullptr && root->right == nullptr)
+      root = nullptr;
+    return root;
+  }
+};
